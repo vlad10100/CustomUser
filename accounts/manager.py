2b482d54204dd757f5ba549792f721b3f586776f
vlad10100 <vbucaralde@gmail.com>
2022-01-25 05:49:41 +0800

    Custom User scratch

---
+++ accounts/manager.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import BaseUserManager 
+
+from django.utils.translation import gettext_lazy as _          # ?????
+
+
+
+class CustomUserManager(BaseUserManager):
+    """ 
+    Custom User Manager 
+    Email is the unique identifier for the authentication.
+    """
+
+    # Create an ordinary User
+    def create_user(self, username, email, password, **extra_fields):
+        if not email:
+            raise ValueError('You need to provide an Email address.')
+         
+        email = self.normalize_email(email)                                 # ??????
+        user = self.model(email=email, username=username, **extra_fields)   # ????
+        user.set_password(user.password)                                         # ?????     
+        user.save()
+        return user
+
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must be assigned to as_staff=True'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must be assigned to as_superuser=True'))
+        
+        return self.create_user(username, email, password, **extra_fields)
+        
+
+
+
+
+
---
+++ accounts/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+
+from django.views.generic import CreateView, TemplateView
+
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView 
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import CustomUser 
+
+class UserSignUpView(CreateView):
+    template_name = 'signup_page.html'
+    model = CustomUser
+    fields = ['first_name', 'last_name', 'birthday',
+            'username', 'email', 'password']
+
+    def get_success_url(self):
+        return reverse_lazy('accounts:login_page')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.save()
+        return super(UserSignUpView, self).form_valid(form)
+    
+
+
+
+class UserLogInView(LoginView):
+    template_name = 'login_page.html'
+    fields = '__all__'
+    
+    def get_success_url(self):
+        return reverse_lazy('accounts:home_page')
+
+
+class HomePage(TemplateView):
+    template_name = 'home_page.html'
+
+    def get_success_url(self):
+        return reverse_lazy('accounts:home_page')
